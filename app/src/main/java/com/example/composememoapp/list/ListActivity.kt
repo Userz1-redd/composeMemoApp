6c5e5612b964df1db199dcddb9f51d134495d01b
jihoon <credit_@naver.com>
2022-06-07 16:37:12 +0900

    feature : Title, TextField

--- app/src/main/java/com/example/composememoapp/list/ListActivity.kt
+++ app/src/main/java/com/example/composememoapp/list/ListActivity.kt
@@ -3,28 +3,48 @@ package com.example.composememoapp.list
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composememoapp.R
+import com.example.composememoapp.ViewModelFactory
 import com.example.composememoapp.data.Memo
+import com.example.composememoapp.data.source.MemoDataSource
+import com.example.composememoapp.data.source.MemoRepository
+import com.example.composememoapp.data.source.local.MemoDatabase
+import com.example.composememoapp.data.source.local.MemoLocalDataSource
 import com.example.composememoapp.ui.theme.ComposeMemoAppTheme
 
-class MainActivity : ComponentActivity() {
+class ListActivity : ComponentActivity() {
+
+    private val database by lazy {
+        MemoDatabase.getDatabase(this)
+    }
+    private val repository by lazy {
+        MemoRepository(MemoLocalDataSource(database.memoDao()))
+    }
+
+    private val viewModel: ListViewModel by viewModels {
+        ViewModelFactory(repository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeMemoAppTheme {
-                ListComposable()
+                ListComposable(viewModel)
             }
         }
     }
@@ -32,43 +52,90 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable()
-fun ListComposable(){
+fun ListComposable(viewModel : ListViewModel){
     Surface(modifier = Modifier.background(Color.White)){
-        
-        title()
-        
-       // memoTextField()
-        
-       // memoList()
+        Column {
+            titleField()
+
+            memoTextField(viewModel)
+
+            memoList()
+        }
     }
 }
 
 @Composable
-fun title(){
+fun titleField(){
     Row(modifier = Modifier
         .padding(10.dp)
         .fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ){
-        Icon(painter = painterResource(id = R.drawable.ic_checkbox), contentDescription = "Check Box")
-        Text()
+        Icon(painter = painterResource(id = R.drawable.ic_checkbox), contentDescription = "Check Box",modifier = Modifier.size(50.dp))
+        Text(
+            text = stringResource(id = R.string.title_text),
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            modifier= Modifier
+                .padding(horizontal = 10.dp)
+                .padding(top = 10.dp)
+            )
     }
 }
 
+@Composable
+fun memoTextField(viewModel : ListViewModel){
+    Row(modifier = Modifier.fillMaxWidth().height(70.dp)) {
+        var textState by remember { mutableStateOf("") }
+        val clicked = {
+            viewModel.addMemo(Memo(textState, false))
+            textState = ""
+        }
+        OutlinedTextField(
+            modifier = Modifier.padding(horizontal = 10.dp).fillMaxHeight(),
+            singleLine = true,
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text("입력창") },
+            placeholder = { Text("메모를 추가하세요!") }
+        )
+        OutlinedButton(
+            modifier = Modifier.padding(10.dp).fillMaxHeight(),
+            onClick = { clicked }
+        ) {
+            Text(text = "Add")
+        }
+    }
+
+}
+
+@Composable
+fun memoList(){
+    LazyColumn(){
+
+    }
+}
 
 @Composable()
 fun listItem(memo : Memo){
     Row(){
+        Text(
+            text = memo.title,
+            overflow = TextOverflow.Ellipsis,
 
+            )
     }
 }
 
 
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeMemoAppTheme {
-        ListComposable()
+        ListComposable(viewModel)
     }
 }
+
+ */
--- app/src/main/java/com/example/composememoapp/list/ListViewModel.kt
+++ app/src/main/java/com/example/composememoapp/list/ListViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.composememoapp.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.composememoapp.data.Memo
+import com.example.composememoapp.data.source.MemoDataSource
 import com.example.composememoapp.data.source.MemoRepository
+import kotlinx.coroutines.launch
 
 class ListViewModel(private val repository : MemoRepository) : ViewModel() {
 
+
+    fun loadMemoList(){
+        viewModelScope.launch{
+            repository.loadMemoList(object : MemoDataSource.LoadMemoListCallback{
+                override fun onMemoListLoaded(list: List<Memo>) {
+
+                }
+
+                override fun onDataNotAvailable() {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+    }
+
+    fun addMemo(memo : Memo){
+        viewModelScope.launch{
+            repository.addMemo(memo)
+        }
+    }
 }
